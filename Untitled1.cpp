76547514befffd5077227e0c93c92aed0aebc082
bhanu-0880 <92150533+bhanu-0880@users.noreply.github.com>
2021-10-08 14:07:33 +0530

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a=12,b=10;
+    printf("%d\n",a&b);
+    printf("%d\n",a|b);
+    printf("%d\n",a^b);
+    printf("%d\n",~b);
+    printf("%d\n",a<<b);
+    printf("%d\n",a>>b); 
+}
