744f350c0bd4cccbf5505708d39c80d40cffd859
bhanu-0880 <92150533+bhanu-0880@users.noreply.github.com>
2021-10-08 15:16:52 +0530

    Add files via upload

---
+++ bitwise all operators.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a=75,b=55;
+	printf("%d\n",a|b);
+	printf("%d\n",a&b);
+	printf("%d\n",a^b);
+	printf("%d\n",~b);
+	printf("%d\n",a<<1);
+	printf("%d\n",a>>1);
+}
