fc8cc387be2f857f11ac03bf9abd9854a9a2c9b6
bhanu-0880 <92150533+bhanu-0880@users.noreply.github.com>
2021-10-08 15:10:55 +0530

    Add files via upload

---
+++ relational.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int a=2,b=10;
+	printf("a=%d b=%d\n",a,b);
+	printf("%d\n",a+b);
+	printf("%d\n",a-b);
+	printf("%d\n",a*b);
+	printf("%d\n",a/b);
+	printf("%d\n",a%b);
+}
