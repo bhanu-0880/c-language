6b23bc6e916b2a0a72a782e37765526972176d7e
bhanu-0880 <92150533+bhanu-0880@users.noreply.github.com>
2021-10-09 13:22:34 +0530

    Update Untitled1.cpp

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -2,10 +2,9 @@
 int main()
 {
 	int a=12,b=10;
-    printf("%d\n",a&b);
-    printf("%d\n",a|b);
-    printf("%d\n",a^b);
-    printf("%d\n",~b);
-    printf("%d\n",a<<b);
-    printf("%d\n",a>>b); 
+    printf("%d\n",a+b);
+    printf("%d\n",a-b);
+    printf("%d\n",a*b);
+    printf("%d\n",a/b);
+    printf("%d\n",a%b); 
 }
