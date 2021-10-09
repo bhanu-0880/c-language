d75f698a3332b751f024ecb58f8f8f98e631bb49
bhanu-0880 <92150533+bhanu-0880@users.noreply.github.com>
2021-10-09 14:08:53 +0530

    Add files via upload

---
+++ PERSONAL DETAILS.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	printf("PERSONAL DETAILS-\n");
+	printf("NAME:K.BHANU PRAKASH\n");
+	printf("MOTHER NAME:K.KRISHNA VENI\n");
+	printf("PLACE OF  BIRTH:RAJAHMUNDRY\n");
+	printf("DATE OF BIRTH:21 JULY 2001\n");
+	printf("QUALIFICATION: B.TECH CIVIL ENIGINEERING\n");
+	printf("COLLEGE:ADITYA ENGINNERING COLEGE\n");
+	printf("ROLL NO:18A91A0122\n");
+}
