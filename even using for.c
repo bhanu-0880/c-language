9b4a91abb08f3ca5b26b7b20fe1274de571fc2ed
bhanu-0880 <92150533+bhanu-0880@users.noreply.github.com>
2021-10-21 14:05:23 +0530

    Add files via upload

---
+++ even using for.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int i,n=10;
+	for(i=6;i<=10;i++)
+	{
+	 if(i%2==0)
+	 {
+	 printf("%d is even\n",i);
+     } 
+     else
+     printf("%d not even\n",i);
+    }
+}
