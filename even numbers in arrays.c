91a7fa3177121946b58700ff2031cce82a9a2747
bhanu-0880 <92150533+bhanu-0880@users.noreply.github.com>
2021-11-12 14:42:30 +0530

    Add files via upload

---
+++ even numbers in arrays.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int a[5]={23,45,56,78,87},sum=1,i;
+	for(i=0;i<5;i++)
+	{
+        if(a[i]%2==0)
+        {
+       		printf("a[%d]=%d\n",i,a[i]);
+
+		}
+	}
+    		
+}
